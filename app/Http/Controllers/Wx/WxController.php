ec4d9fc736b77682944e1a03ae78a42c09bb1a29
root <root@PC-20210426WVZT.localdomain>
2021-10-12 16:41:35 +0800

    测试

--- app/Http/Controllers/Wx/WxController.php
+++ app/Http/Controllers/Wx/WxController.php
@@ -13,7 +13,13 @@ class WxController extends Controller
     //获取用户登录信息
     public function codeSession(Request $request)
     {
-        var_dump('雷海荣');
+        $code = $request->input('code');
+        $url=sprintf(config('wechatUrl.url'),config('wechatUrl.appid'),config('wechatUrl.secret'),$code);
+        $client=new Client(['timeout'=>5,'verify'=>false]);
+        $res=$client->get($url);
+        $data=(string)$res->getBody();
+        $arr=json_decode($data,true);
+        return ['code'=>200,'msg'=>'ok','data'=>$arr];
 
     }
 
