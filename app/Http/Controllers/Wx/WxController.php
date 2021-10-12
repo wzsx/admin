cdea505a32483fbfed715acd88ac04d238d4e34d
root <root@PC-20210426WVZT.localdomain>
2021-10-12 14:16:52 +0800

    修改

--- app/Http/Controllers/Wx/WxController.php
+++ app/Http/Controllers/Wx/WxController.php
@@ -14,13 +14,14 @@ class WxController extends Controller
     //获取用户登录信息
     public function code2Session(Request $request)
     {
-        $code = $request->input('code');
-//        $code = '083s76100yJGAM1XrQ300iFcDb3s761W';
-        $url=sprintf(config('wechatUrl.url'),config('wechatUrl.appid'),config('wechatUrl.secret'),$code);
-        $client=new Client(['timeout'=>5,'verify'=>false]);
-        $res=$client->post($url);
-        $data=(string)$res->getBody();
-        $arr=json_decode($data,true);
-        return ['code'=>200,'msg'=>'ok','data'=>$arr];
+        var_dump(111);
+//        $code = $request->input('code');
+////        $code = '083s76100yJGAM1XrQ300iFcDb3s761W';
+//        $url=sprintf(config('wechatUrl.url'),config('wechatUrl.appid'),config('wechatUrl.secret'),$code);
+//        $client=new Client(['timeout'=>5,'verify'=>false]);
+//        $res=$client->post($url);
+//        $data=(string)$res->getBody();
+//        $arr=json_decode($data,true);
+//        return ['code'=>200,'msg'=>'ok','data'=>$arr];
     }
 }
