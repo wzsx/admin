9150f0b8adb827f57ab472bccb68294989216dd7
root <root@PC-20210426WVZT.localdomain>
2021-10-12 17:14:16 +0800

    优化

--- app/Http/Controllers/Wx/WxController.php
+++ app/Http/Controllers/Wx/WxController.php
@@ -17,9 +17,10 @@ class WxController extends Controller
         $url=sprintf(config('wechatUrl.url'),config('wechatUrl.appid'),config('wechatUrl.secret'),$code);
         $client=new Client(['timeout'=>5,'verify'=>false]);
         $res=$client->post($url);
-        $data=(string)$res->getBody();
-        $arr=json_decode($data,true);
-        return ['code'=>200,'msg'=>'ok','data'=>$arr];
+        var_dump($res);
+//        $data=(string)$res->getBody();
+//        $arr=json_decode($data,true);
+//        return ['code'=>200,'msg'=>'ok','data'=>$arr];
 
     }
 
