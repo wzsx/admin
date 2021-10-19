5f36be54938ba8a928d0209dbe906c7dc78509df
root <root@PC-20210426WVZT.localdomain>
2021-10-19 16:20:10 +0800

    微信授权

--- app/Http/Controllers/Wx/WxController.php
+++ app/Http/Controllers/Wx/WxController.php
@@ -179,6 +179,7 @@ class WxController extends Controller
 
         $data['session3rd'] = $session3rd;
         cache($session3rd, $data['openId'] . $session_key);
+        var_dump($data);
         return ['code'=>200,'msg'=>'ok','data'=>$data];
     }
 
