0e36fd71b062d1b0d9b323cfb718aff8a1fe07d1
root <root@PC-20210426WVZT.localdomain>
2021-10-22 15:34:04 +0800

    修改并添加购物车

--- app/Http/Controllers/Cart/CartController.php
+++ app/Http/Controllers/Cart/CartController.php
@@ -45,9 +45,9 @@ class CartController extends Controller
         $params = $request->all();
         $mid = $params['mid'];
         $goods_id = $params['goods_id'];
-        $goods_num = $params['goods_num'];
-        $is_selected = $params['is_selected'];
-        if (!$goods_id || !$mid || !$is_selected) {
+        $goods_num = $params['goods_num']??0;
+        $is_selected = $params['is_selected']??0;
+        if (!$goods_id || !$mid) {
             return ['code' => 500001, 'msg' => '缺少必要参数'];
         }
         if($goods_num==0){
@@ -60,9 +60,9 @@ class CartController extends Controller
             $update_at = date('Y-m-d H:i:s');
             $update = CartModel::query()->where(['mid'=>$mid,'goods_id'=>$goods_id])->update(['goods_num'=>$goods_num,'update_at'=>$update_at,'is_selected'=>$is_selected]);
             if($update){
-                return ['code' => 0, 'msg' => '加入购物车成功','data'=>[]];
+                return ['code' => 0, 'msg' => '成功','data'=>[]];
             }
-            return ['code' => 200001, 'msg' => '添加失败'];
+            return ['code' => 200001, 'msg' => '操作失败'];
     }
     //购物车列表
     public function cartList(Request $request)
@@ -74,11 +74,13 @@ class CartController extends Controller
         }
         $goods = CartModel::query()->where(['mid' => $mid, 'status' => 0])->select(['id','goods_id','goods_num','is_selected'])->get()->toArray();
         $goods_id = array_column($goods, 'goods_id');
+        $goods_on = $goods;
         $goods_info = GoodsModel::query()->whereIn('goods_id', $goods_id)->where(['if_disable'=>0])->select('goods_id', 'goods_name', 'goods_lord_img', 'goods_price', 'goods_size')->get()->toArray();
         $goodsArr = array_column($goods_info,'goods_id','goods_id');
         $failure_goods = GoodsModel::query()->whereIn('goods_id', $goods_id)->where(['if_disable'=>1])->select('goods_id', 'goods_name', 'goods_lord_img', 'goods_price', 'goods_size')->get()->toArray();
         $goodsRes = array_column($goods_info,null,'goods_id');
-//        var_dump($goodsArr);
+        $failuresArr = array_column($failure_goods,'goods_id','goods_id');
+        $failuresRes = array_column($failure_goods,null,'goods_id');
         foreach ($goods as $k=>&$v){
             if (!isset($goodsArr[$v['goods_id']])) {
                 unset($goods[$k]);
@@ -89,7 +91,17 @@ class CartController extends Controller
             $v['goods_price']=$goodsRes[$v['goods_id']]['goods_price'];
             $v['goods_size']=$goodsRes[$v['goods_id']]['goods_size'];
         }
-        return ['code' => 0, 'msg' => '成功','data'=>['valid'=>$goods,'failure'=>[]]];
+        foreach ($goods_on as $a=>&$s){
+            if (!isset($failuresArr[$s['goods_id']])) {
+                unset($goods_on[$a]);
+                continue;
+            }
+            $s['goods_name']=$failuresRes[$s['goods_id']]['goods_name'];
+            $s['goods_lord_img']=$failuresRes[$s['goods_id']]['goods_lord_img'];
+            $s['goods_price']=$failuresRes[$s['goods_id']]['goods_price'];
+            $s['goods_size']=$failuresRes[$s['goods_id']]['goods_size'];
+        }
+        return ['code' => 0, 'msg' => '成功','data'=>['valid'=>array_values($goods),'failure'=>array_values($goods_on)]];
 
 
     }
