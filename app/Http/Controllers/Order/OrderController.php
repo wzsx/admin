c7dbd8a23d4c9314555462ff53bdac972ba090cf
root <root@PC-20210426WVZT.localdomain>
2021-11-05 14:24:45 +0800

    修改预订单信息

--- app/Http/Controllers/Order/OrderController.php
+++ app/Http/Controllers/Order/OrderController.php
@@ -25,6 +25,7 @@ class OrderController extends Controller
         $phone = $params['order_phone'];
 //        $commodity = json_decode($params['commodity'],true);//所有商品信息
         $commodity=$params['commodity'];
+//        $commodity = [['goods_id'=>697239,'goods_img'=>1111,'goods_price'=>39,'number'=>1,'goods_name'=>'清补凉','goods_size'=>1000]];
         $gross_price = $params['gross_price'];
         $total_price = $params['total_price'];
         $desc = $params['desc'];//留言
@@ -43,11 +44,6 @@ class OrderController extends Controller
         $sum = 0;
         foreach ($commodity as $key =>$v) {
             $price = GoodsModel::query()->where(['goods_id'=>$v['goods_id']])->value('goods_price');
-//            return ['code'=>$price,'data'=>$v['goods_id'],'msg'=>$v['goods_price']];
-//            if($price!=$v['goods_price']){
-//                return ['msg'=>'不对'];
-//            }
-//            return ['code'=>$price,'data'=>$v['goods_id'],'msg'=>$v['goods_price']];
             if($price!=$v['goods_price']) {
                 return ['code' => 500003, 'msg' => '商品价格不符,请联系客服'];
             }elseif (($price*$v['number'])!=($v['goods_price']*$v['number'])){
@@ -63,16 +59,16 @@ class OrderController extends Controller
         $str = 'FXT'.date('Ymd').substr(implode(NULL, array_map('ord', str_split(substr(uniqid(), 7, 13), 1))), 0, 8);
         $openid = ShopUserModel::query()->where(['mid'=>$mid])->value('openid');
         $create_at = date('Y-m-d H:i:s');
-        $order = OrderModel::query()->insert(['order_no'=>$str,'openid'=>$openid,'mid'=>$mid,'freight_price'=>$freight_price,'goods_price'=>$gross_price,'total_price'=>$total_price,'desc'=>$desc,'create_at'=>$create_at,'receiver_address'=>$receiver_address,'order_goods_num'=>$order_goods_num,'order_phone'=>$phone,'order_name'=>$name,'coupon_info'=>$coupon_info,'coupon_cut'=>$coupon_cut,'coupon_status'=>$coupon_status]);
+        $order = OrderModel::query()->insert(['order_no'=>$str,'openid'=>$openid,'mid'=>$mid,'freight_price'=>$freight_price,'gross_price'=>$gross_price,'total_price'=>$total_price,'desc'=>$desc,'create_at'=>$create_at,'receiver_address'=>$receiver_address,'order_goods_num'=>$order_goods_num,'order_phone'=>$phone,'order_name'=>$name,'coupon_info'=>$coupon_info,'coupon_cut'=>$coupon_cut,'coupon_status'=>$coupon_status]);
         $dis_price = $gross_price - $coupon_cut;
         //写入订单商品表
         $carousel = [];
         foreach ($commodity as $key =>$v) {
-            $carousel = OrderGoodsModel::query()->insert(['mid'=>$mid,'order_no'=>$str,'goods_id'=>$v['goods_id'],'goods_title'=>$v['goods_name'],'goods_size'=>$v['goods_size'],'goods_img'=>$v['goods_img'],'selling_price'=>$v['goods_price'],'number'=>$v['number'],'create_at'=>$create_at,'dis_price'=>$dis_price]);
+            $carousel = OrderGoodsModel::query()->insert(['mid'=>$mid,'order_no'=>$str,'goods_id'=>$v['goods_id'],'goods_name'=>$v['goods_name'],'goods_size'=>$v['goods_size'],'goods_img'=>$v['goods_img'],'selling_price'=>$v['goods_price'],'number'=>$v['number'],'create_at'=>$create_at,'dis_price'=>$dis_price]);
         }
         if($carousel || $order){
-//            $job = (new OrderStatus($str))->delay(900);
-//            $this->dispatch($job);
+            $job = (new OrderStatus($str))->delay(900);
+            $this->dispatch($job);
             return ['code' => 0, 'msg' => '生成预订单成功','data'=>['order_no'=>$str]];
         }
     }
