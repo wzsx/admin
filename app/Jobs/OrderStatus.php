6565f0aa85e57e08f5cfa8b1e630eef2cc28bf34
root <root@PC-20210426WVZT.localdomain>
2021-11-05 15:04:00 +0800

    修改预订单信息

--- app/Jobs/OrderStatus.php
+++ app/Jobs/OrderStatus.php
@@ -37,11 +37,11 @@ class OrderStatus implements ShouldQueue
     {
         //
         $user = $this->order_no;
-        GoodsModel::query()->where(['goods_id'=>$user])->update(['goods_name'=>'薄荷清茶']);
-//        $status = OrderModel::query()->where(['order_no'=>$user])->pluck('is_pay');
-//        if($status!=1){
-//            OrderModel::query()->where(['order_no'=>$user])->update(['status'=>0,'is_deleted'=>1]);
-//            OrderGoodsModel::query()->where(['order_no'=>$user])->update(['is_deleted'=>1]);
-//        }
+//        GoodsModel::query()->where(['goods_id'=>$user])->update(['goods_name'=>'薄荷清茶']);
+        $status = OrderModel::query()->where(['order_no'=>$user])->value('is_pay');
+        if($status!=1){
+            OrderModel::query()->where(['order_no'=>$user])->update(['status'=>0,'is_deleted'=>1]);
+            OrderGoodsModel::query()->where(['order_no'=>$user])->update(['is_deleted'=>1]);
+        }
     }
 }
