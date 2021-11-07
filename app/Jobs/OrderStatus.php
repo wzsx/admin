b65bba03b6d05321c684b04e2314b4203b4e7b79
root <root@PC-20210426WVZT.localdomain>
2021-11-07 17:05:48 +0800

    修改时间

--- app/Jobs/OrderStatus.php
+++ app/Jobs/OrderStatus.php
@@ -37,9 +37,9 @@ class OrderStatus implements ShouldQueue
     {
         //
         $user = $this->order_no;
-        var_dump($user);
+//        var_dump($user);
         $status = OrderModel::query()->where(['order_no'=>$user])->value('is_pay');
-        var_dump($status);
+        var_dump($status.$user);
         if($status!=1){
             OrderModel::query()->where(['order_no'=>$user])->update(['status'=>0,'is_deleted'=>1]);
             OrderGoodsModel::query()->where(['order_no'=>$user])->update(['is_deleted'=>1]);
