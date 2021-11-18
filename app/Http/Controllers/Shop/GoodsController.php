4289f36eafb04f18f666ef3c22c4351b9a9be297
root <root@PC-20210426WVZT.localdomain>
2021-11-18 16:28:43 +0800

    商品上下架与编辑

--- app/Http/Controllers/Shop/GoodsController.php
+++ app/Http/Controllers/Shop/GoodsController.php
@@ -97,16 +97,9 @@ class GoodsController extends Controller
     //编辑后台商品
     public function adminUpdateGoods(Request $request){
         $params = $request->all();
-        if (empty($params['goods_id'])||empty($params['goods_name'])||empty($params['goods_lord_img'])||empty($params['goods_about'])||empty($params['goods_details'])||empty($params['goods_size'])||empty($params['goods_price'])||empty($params['goods_cate'])||isset($params['if_show'])) {
+        if (empty($params['goods_id'])||empty($params['goods_name'])||empty($params['goods_carousel'])||empty($params['goods_lord_img'])||empty($params['goods_about'])||empty($params['goods_details'])||empty($params['goods_size'])||empty($params['goods_price'])||empty($params['goods_cate'])||isset($params['if_show'])) {
             return ['code' => 30001, 'msg' => '缺少必要参数'];
         }
-        if(empty($params['goods_carousel'])){
-            $goods = GoodsModel::query()->where(['goods_id'=>$params['goods_id']])->update(['goods_name'=>$params['goods_name'],'goods_lord_img'=>$params['goods_lord_img'],'goods_about'=>$params['goods_about'],'goods_details'=>$params['goods_details'],'goods_size'=>$params['goods_size'],'goods_price'=>$params['goods_price'],'goods_cate'=>$params['goods_cate'],'if_show'=>$params['if_show']]);
-           if($goods){
-               return ['code' => 0, 'msg' => '修改成功','data'=>[]];
-           }
-            return ['code' => 20500, 'msg' => '修改失败','data'=>[]];
-        }else {
             $goods_id = $params['goods_id'];
             $goods_carousel = $params['goods_carousel'];//轮播图
             GoodsCarouselModel::query()->where(['carousel_id' => $goods_id])->delete();
@@ -119,6 +112,5 @@ class GoodsController extends Controller
             }
             return ['code' => 20500, 'msg' => '修改失败','data'=>[]];
         }
-    }
 }
 ?>
